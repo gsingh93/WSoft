fb5cb35eff2a36ecef1367f00e1e91bc8dd87d0d
unknown <Jordan@Jordan-PC.(none)>
2012-01-12 18:46:35 -0500

    added new obstacles and acceleromater

--- scripts/main.lua
+++ scripts/main.lua
@@ -6,6 +6,22 @@ local function generateTree(n)
 	tree.n.x = x
 	tree.n.y = y
 end
+-- Random tramp generator
+local function generateTramp(n)
+	tramp.n = display.newImage("../images/trampolineLOW.png")
+	x = math.random(tramp.n.contentWidth, display.contentWidth-tramp.n.contentWidth)
+	y = math.random(-display.contentHeight+tramp.n.contentHeight, -tramp.n.contentHeight)
+	tramp.n.x = x
+	tramp.n.y = y
+end
+-- Random sandBox generator
+local function generateSandBox(n)
+	sandBox.n = display.newImage("../images/sandboxLOW.png")
+	x = math.random(sandBox.n.contentWidth, display.contentWidth-sandBox.n.contentWidth)
+	y = math.random(-display.contentHeight+sandBox.n.contentHeight, -sandBox.n.contentHeight)
+	sandBox.n.x = x
+	sandBox.n.y = y
+end
 	
 -- Scroll backgrounds and trees
 local function scroll(event)
@@ -42,6 +58,22 @@ local function scroll(event)
 			generateTree(n)
 		end
 	end
+		-- Scroll the tramps and if a tramp reaches the bottom, remove it and generate another tramp
+	for i = 0, n do
+		tramp.n.y = tramp.n.y + tramp.velocity
+		if tramp.n.y >= display.contentHeight+50 then
+			tramp.n:removeSelf()
+			generateTramp(n)
+		end
+	end
+	
+	for i = 0, n do
+		sandBox.n.y = sandBox.n.y + sandBox.velocity
+		if sandBox.n.y >= display.contentHeight+50 then
+			sandBox.n:removeSelf()
+			generateSandBox(n)
+		end
+	end
 end
 
 -- Hide the status bar
@@ -75,11 +107,17 @@ background3.x = 0
 
 -- Creates n trees. TODO: Create a dynamic number of trees and stop overlap of trees
 tree = {}
+tramp = {}
+sandBox = {}
 n = 3
 for i= 1, n do
 	generateTree(n)
+	generateTramp(n)
+	generateSandBox(n)
 end
 tree.velocity = 2
+tramp.velocity = 2
+sandBox.velocity = 2
 
 -- Create a box and center it
 box = display.newImage("../images/box.jpg", display.contentWidth/2, display.contentHeight - 20)
@@ -96,7 +134,13 @@ box.velocity.z = 40
 -- Set the value of gravity
 gravity = 1
 
-Runtime:addEventListener("enterFrame", scroll)
-
 -- Change by jordan
+-- Accelerometer
+local function onTilt( event )
+	box.x = box.x + (10 * event.xGravity)
+	--box.y = box.y + (35 * event.yGravity * -1)
+end
+
+Runtime:addEventListener("enterFrame", scroll)
+Runtime:addEventListener("accelerometer", onTilt)
 
